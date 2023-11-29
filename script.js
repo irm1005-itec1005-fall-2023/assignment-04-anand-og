c9ca6acc03ecb525d517e98512849026ae058a25
Anand Ogungbemile <anand.og8@gmail.com>
2023-11-29 17:55:26 -0500

    rough java

--- script.js
+++ script.js
@@ -8,7 +8,11 @@
 //
 // Variables
 //
-
+let todoItems = [
+];
+let todoItemsForm = document.getElementById("todoItemform");
+let todoItemsList = document.getElementById("todoList");
+let todoInput = document.getElementById("todoInput");
 // Constants
 const appID = "app";
 const headingText = "To do. To done. ✅";
@@ -18,6 +22,82 @@ let appContainer = document.getElementById(appID);
 
 //
 // Functions
+todoItemsForm.addEventListener ("submit", addTodoItem);
+
+function addTodoItem (event) {
+  event.preventDefault();
+
+  todoItems.push(todoInput.value)
+  console.log("addTodoItem", todoItems);
+
+  todoInput.value = "";
+
+  renderItems();
+}
+
+function renderItems() {
+  todoItemsList.innerHTML = "";
+
+  for (let i=0; i < todoItems.length; i++) {
+    
+    let tempListItem = document.createElement ("li");
+    
+    tempListItem.textContent = todoItems[i];
+
+    todoItemsList.appendChild(tempListItem);
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "delete task";
+    deleteBtn.dataset.index = i;
+
+    deleteBtn.addEventListener("click", function(event){
+      todoItems.splice(event.target.dataset.index, 1);
+      renderItems();
+    })
+
+
+    tempListItem.appendChild(deleteBtn);
+
+    let completeBtn = document.createElement('button');
+    completeBtn.textContent = "Done!";
+    completeBtn.dataset.complete = i;
+
+    completeBtn.addEventListener("click", function(event){
+      todoItemscompleted = true;
+
+      console.log(todoItemscompleted)
+    })
+
+    tempListItem.appendChild(completeBtn);
+
+    for (let i=0; 1 < todoItems.length; i++) {
+      if (todoItemscompleted = true){
+        
+      }
+
+    }
+    
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //
 
 // Add a heading to the app container
